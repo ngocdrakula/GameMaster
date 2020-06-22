e5963af7133e20b5a4a06483c94d711cacd99f08
ngocdrakula@gmail.com <ngocdrakula@gmail.com>
2020-06-23 00:04:02 +0700

    test

--- src/App.js
+++ src/App.js
@@ -1,27 +1,27 @@
 import React from 'react';
 
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
+// import {
+//   BrowserRouter as Router,
+//   Switch,
+//   Route,
 
-} from "react-router-dom";
-import Navbar from './modules/Navbar';
+// } from "react-router-dom";
+// import Navbar from './modules/Navbar';
 
-import Game from './components/Game';
-import ErrorPage from './components/ErrorPage';
-import Home from './components/Home';
+// import Game from './components/Game';
+// import ErrorPage from './components/ErrorPage';
+// import Home from './components/Home';
 
 
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './gamemaster.css';
+// import 'bootstrap/dist/css/bootstrap.min.css';
+// import './gamemaster.css';
 import './App.css';
 
 export default class App extends React.Component {
   render() {
     return (
       <React.Fragment>
-        <Navbar />
+        {/* <Navbar />
         <div className="container-md container-xs">
           <Router>
             <Switch>
@@ -30,7 +30,8 @@ export default class App extends React.Component {
               <Route path="/"component={Home}/>
             </Switch>
           </Router>
-        </div>
+        </div> */}
+        this is app
       </React.Fragment>
     );
   }
